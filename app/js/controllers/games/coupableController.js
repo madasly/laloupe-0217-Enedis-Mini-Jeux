56ce062da0d8e29523d0f5b749e3d8758fe96960
Cleclemi28 <c.trofleau@gmail.com>
2017-04-22 18:53:23 +0200

    fonctionalité ok, il reste plus que les bulle a faires

--- app/js/controllers/games/coupableController.js
+++ app/js/controllers/games/coupableController.js
@@ -1,4 +1,12 @@
 angular.module('app')
     .controller('coupableController', function($scope) {
-      /* Here is your main controller */
+
+
+      $scope.vrai = function vrai(){
+        console.log("bonne réponse");
+      };
+      $scope.faux = function faux(){
+        console.log("Mauvaise réponse");
+      };
+
     });
