d522b1a1c83c99f22f23822b35c23477af070159
Cleclemi28 <c.trofleau@gmail.com>
2017-05-09 15:49:04 +0200

    ajout d'animation -refonte graphique -intégration des bulles

--- app/js/controllers/games/miniQuizController.js
+++ app/js/controllers/games/miniQuizController.js
@@ -1,10 +1,34 @@
 angular.module('app')
-    .controller('miniQuizController', function($scope) {
-      $scope.$parent.nomDuJeu = "Mini Quizz";
-      $scope.vrai = function vrai(){
-        console.log("bravo");
-      };
-      $scope.faux = function faux(){
-        console.log("mauvaise reponse");
-      };
-    });
+  .controller('miniQuizController', function($scope) {
+
+    // Variable d'information général du jeu
+    $scope.$parent.nomDuJeu = "Mini Quizz";
+    $scope.$parent.theme = "Ville";
+
+    //Function en cas de bonne réponses
+    $scope.vrai = function vrai() {
+      $scope.showBulle({
+        show: true,
+        character: "Greg",
+        content: "Félicitation tu a bien répondu a la question !!"
+      });
+    };
+
+    // Si l'utilisateur clic sur le bouton 2
+    $scope.faux1 = function faux() {
+      $scope.showBulle({
+        show: true,
+        character: "Lea",
+        content: "Non !! Surtout ne touche pas a la branche !! tu risque l'électrocution voir meme dans le pire des cas la mort !!"
+      });
+    };
+
+    // Si l'utilisateur clic sur le bouton 3
+    $scope.faux = function faux() {
+      $scope.showBulle({
+        show: true,
+        character: "Lea",
+        content: "Mauvaise réponse. Si personne ne prévient Énedis il y as encore plus de risque qu'il y ai des accidents."
+      });
+    };
+  });
