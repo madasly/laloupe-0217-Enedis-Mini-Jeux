792bb50d45f7c67767bfb4200d180ebcef9e6de8
Cleclemi28 <c.trofleau@gmail.com>
2017-05-15 00:18:10 +0200

    jeu terminé, reste bug de noeud dans le fichier svg.html

--- app/js/controllers/games/filGregController.js
+++ app/js/controllers/games/filGregController.js
@@ -1,12 +1,25 @@
 angular.module('app')
     .controller('filGregController', function($scope) {
 
-      $scope.badrep = function(){
+      $scope.$parent.nomDuJeu = "Le Fil de Greg";
+      $scope.$parent.theme = "Maison";
+
+      $scope.faux = function(){
         console.log("mauvaise réponse");
+        $scope.showBulle({
+          show: true,
+          character: "Lea",
+          content: "Non !! ce fil n'est vraiment pas en bonne état"
+        });
       };
 
-      $scope.goodrep = function(){
+      $scope.vrai = function(){
         console.log("bonne réponse");
+        $scope.showBulle({
+          show: true,
+          character: "Greg",
+          content: "Félicitation! tu as bien répondu à la question !!"
+        });
       };
 
     });
