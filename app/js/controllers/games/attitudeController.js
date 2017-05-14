51efa62b091f373b4e6663c7ca8f02612298c979
Cleclemi28 <c.trofleau@gmail.com>
2017-05-15 00:40:26 +0200

    fin de l'algo du jeu il manque les images isolé ainsi que les anim

--- app/js/controllers/games/attitudeController.js
+++ app/js/controllers/games/attitudeController.js
@@ -1,5 +1,25 @@
 angular.module('app')
     .controller('attitudeController.js', function($scope) {
 
+      $scope.$parent.theme = "Maison";
+      $scope.$parent.nomDuJeu = "La Bonne Attitude";
+
+      $scope.vrai = function(){
+        console.log("Bonne réponse");
+        $scope.showBulle({
+          show: true,
+          character: "Greg",
+          content: "Félicitation, c'est la bonne réponse"
+        });
+      };
+
+      $scope.faux = function(){
+        console.log("... Sérieux !! ?");
+        $scope.showBulle({
+          show: true,
+          character: "Lea",
+          content: "Non ! La bonne attitude pour les multiprise est de ne pas les superposer !"
+        });
+      };
 
     });
