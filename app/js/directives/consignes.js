b8879e624a7ee9db73f4881083fd4f783f0856ae
maallard <mar.allard@gmail.com>
2017-05-16 15:48:49 +0200

    Ajout des consignes

---
+++ app/js/directives/consignes.js
@@ -0,0 +1,11 @@
+angular.module('app')
+    .directive('consignes', function() {
+        return {
+            restrict: 'A',
+            replace: true,
+            templateUrl: 'anon/directives/consignes.html',
+            scope: {
+                texte: "="
+            },
+        };
+    });
