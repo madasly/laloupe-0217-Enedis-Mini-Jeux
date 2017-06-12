c858cd8a8c45b4cdd093414e0efe5c37e06ec64b
madasly <madasly@gmail.com>
2017-06-12 23:41:07 +0200

    tomberpanneau map ok responsive

---
+++ app/js/directives/games/tomber.js
@@ -0,0 +1,8 @@
+angular.module('app')
+    .directive('tomber', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'anon/directives/games/tomber.svg.html',
+        };
+    });
